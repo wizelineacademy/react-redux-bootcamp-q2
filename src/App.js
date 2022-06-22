486fc5aeabf828af3f5fd7e14d45d5e638bff6e1
Cesar Romero Villarreal <89871050+cesarwizeline@users.noreply.github.com>
2022-06-22 16:35:58 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import GlobalStyle from './styles/globalStyles';
+import {AppRouter} from './routes/AppRouter';
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <AppRouter />
+    </>
+  );
+}
+
+export default App;
---
+++ src/styles/pages/Home.styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Main = styled.div`
+  display: flex;
+  height: 500px;
+  flex-direction: column;
+  align-items: center;
+  justify-content:center
+`;
