f84c8859f4802dc8516bdc124fb8c081e6de0efd
wfercanas <fernando.canas@outlook.com>
2022-06-28 22:24:30 -0500

    add/ new global colors

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -1,12 +1,17 @@
-import { createGlobalStyle } from 'styled-components';
- 
+import { createGlobalStyle } from "styled-components";
+
 const GlobalStyle = createGlobalStyle`
+  :root {
+    --r400: #BF2600;
+    --n10: #FAFBFC;
+  }
+
   body {
     margin: 0;
     padding: 0;
     background: white;
     font-family: Open-Sans, Helvetica, Sans-Serif;
   }
-`
- 
+`;
+
 export default GlobalStyle;
