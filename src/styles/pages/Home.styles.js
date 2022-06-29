9baea4c8eb5c5ff2c1df51702c8955239df28214
wfercanas <fernando.canas@outlook.com>
2022-06-29 16:10:31 -0500

    delete/ legacy pages and Home.styles

--- src/styles/pages/Home.styles.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Main = styled.div`
-  display: flex;
-  height: 500px;
-  flex-direction: column;
-  align-items: center;
-  justify-content:center
-`;
