5ea71c01aee36208777ddc7047ead73ebb86e9fc
macbook pro <andresgfranco0@gmail.com>
2022-07-07 20:04:21 -0500

    feat: create new components to render a list of products coming from the mock file

---
+++ src/pages/Products/Products.styles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const ShopNowTitleWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 2%;
+`;
+
+export const ShopNowTitle = styled.h1`
+  position: relative;
+  font-size: 30px;
+  z-index: 1;
+  overflow: hidden;
+  text-align: center;
+  margin: auto;
+`;
+
+export const ProductsWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  gap: 0,5%;
+`;
