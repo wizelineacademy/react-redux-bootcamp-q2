99b0214357a475d05cdae9f531eef878e1b14256
Felipe Rivera <felipe.rivera@toshibagcs.com>
2022-07-09 22:30:22 -0500

    [feat/deliverable2]fix styles cart

--- src/components/Cart/CartStyles.js
+++ src/components/Cart/CartStyles.js
@@ -13,19 +13,46 @@ export const ProductCartContainer = styled.div`
     td{
         padding:16px;
     }
-    
+    hr {
+        margin-top: 16px;
+        margin-bottom: 16px;
+        width: 100%;
+        height: 1px;
+        background-color: rgb(211, 211, 212);
+        border: medium none;
+    }
 `;
 export const SummaryProductCartConatiner = styled.div`
     width: 20%;
     display: inline-block;
     padding:10px; 
     box-sizing: border-box;
+    font-family: "Nunito Sans",sans-serif;
     div.summay{
         padding:32px;
         display:inline-block;
         background-color: rgb(245, 245, 245);
     }
     float:right;
+    div > button {
+        margin-top: 16px;
+        width: 100%;
+        background-color: rgb(32,52,73);
+        color: white;
+        border: 0px none;
+        height: 50px;
+        cursor: pointer;
+        -webkit-transition: all 0.3s ease 0s;
+        transition: all 0.3s ease 0s;
+      }
+    div >  hr {
+        margin-top: 16px;
+        margin-bottom: 16px;
+        width: 100%;
+        height: 1px;
+        background-color: rgb(211, 211, 212);
+        border: medium none;
+    }
 `;
 export const ProductDetails = styled.div`
     display:flex;
